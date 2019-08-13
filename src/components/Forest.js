bbf6082c619f87a3e81cda2d0e0a2b3c6748885c
julenia <julia.jankowska@onet.eu>
2019-08-13 17:33:10 +0200

    displaying mushroomers on the board with user ids

--- src/components/Forest.js
+++ src/components/Forest.js
@@ -26,6 +26,7 @@ class Forest extends React.Component {
         : null
       const space = <span key={location} className='space'>
         {location}
+        {mushroomers.map(mushroomer => mushroomer.userId)}
         {good}
         {bad}
       </span>
@@ -36,7 +37,9 @@ class Forest extends React.Component {
     return <span>
       <h3>{forest.name}</h3>
       {spaces}
+      <div>
       <button className='dieButton'>Roll a die</button>
+      </div>
     </span>
   }
 }
